2253ba6f6de59897a910dcaab2b6f1fab431a0f9
GitWave22 <135379882+GitWave22@users.noreply.github.com>
2024-03-19 00:23:20 +0000

    change value

--- adjustSize.js
+++ adjustSize.js
@@ -1,18 +1,19 @@
-console.log("run")
-
 fetch('output.txt')
   .then(response => response.text())
   .then(data => {
     // This is the text from output.txt
     let textFromFile = data;
-    console.log(textFromFile)
 
-    // Find the div by its ID (replace 'myDiv' with your div's ID)
-    let myDiv = document.getElementById('flows_in');
+    // Find the textarea by its ID (replace 'flows_in' with your textarea's ID)
+    let myTextarea = document.getElementById('flows_in');
 
-    // Replace the text within the div
-    myDiv.innerHTML = textFromFile;
+    // Replace the text within the textarea
+    myTextarea.value = textFromFile;
+
+    // Create a new 'change' event
+    var event = new Event('change');
+
+    // Dispatch it.
+    myTextarea.dispatchEvent(event);
   })
   .catch(error => console.error(error));
-
-
