bda76a1a6692cee8142d5a2f0aa22ada6b511fb8
GitWave22 <135379882+GitWave22@users.noreply.github.com>
2024-03-19 00:09:46 +0000

    console logs

--- adjustSize.js
+++ adjustSize.js
@@ -1,9 +1,11 @@
+console.log("run")
 
 fetch('output.txt')
   .then(response => response.text())
   .then(data => {
     // This is the text from output.txt
     let textFromFile = data;
+    console.log(textFromFile)
 
     // Find the div by its ID (replace 'myDiv' with your div's ID)
     let myDiv = document.getElementById('flows_in');
@@ -12,4 +14,5 @@ fetch('output.txt')
     myDiv.innerHTML = textFromFile;
   })
   .catch(error => console.error(error));
+
 
